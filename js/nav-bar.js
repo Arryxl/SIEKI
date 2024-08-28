740acb551a6d0360277ec2c67b8ed13e2cb407c4
Arryxl <juanreyval99@gmail.com>
2024-08-27 23:36:05 -0500

    cambios al footer

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -7,14 +7,20 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 const nav = document.querySelector(".bar-top")
-var img = document.getElementsByClassName("logo")[0]
+var imgLogo = document.getElementsByClassName("logo")[0]
+var iconShoppingCart = document.getElementsByClassName("shopping_cart_img")[0]
+var iconUser = document.getElementsByClassName("user_img")[0]
 
 window.addEventListener("scroll", function() {
     if (window.scrollY > 0) {
-        img.src = "./assets/icons/LOGO-NEGRO.png";
+        imgLogo.src = "./assets/icons/LOGO-NEGRO.png";
+        iconShoppingCart.src = "./assets/icons/shopping_cart_black.png"
+        iconUser.src = "./assets/icons/account_circle_black.png"
         nav.classList.add("active2");
     } else {
-        img.src = "./assets/icons/LOGOBLANCO.png";
+        imgLogo.src = "./assets/icons/LOGOBLANCO.png";
+        iconShoppingCart.src = "./assets/icons/shopping_cart_white.png"
+        iconUser.src = "./assets/icons/account_circle_white.png"
         nav.classList.remove("active2");
     }
 })
